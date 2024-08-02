90cd9e9c594daf65edba3b7419250b54fdb57644
jessxwu <jessicaxywu@gmail.com>
2024-08-02 19:42:21 +0100

    Revert "header changes - will revert"
    This reverts commit 2d2c8fc6ef79687e22da5b916c35c8aaa0294743.

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -39,37 +39,29 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     title: 'Jessica Wu',
-    // subtitle: 'words, thoughtful digressions',
-    description: 'words, thoughtful digressions',
-
-    logo: {
-        src: "/images/logo.png",  // Path is relative to the 'public' directory
-        alt: "Your Site Logo",
+    subtitle: 'words, thoughtful digressions',
+    description: 'Digital second brain of sorts',
+    image: {
+        src: '/dante-preview.jpg',
+        alt: 'Dante - Astro.js and Tailwind CSS theme'
     },
-    image: { // Assuming this is the default image
-        src: '/images/logo.png',
-        alt: 'Jessica Wu - words, mostly thoughtful digressions'
-    },
-
     headerNavLinks: [
-
         {
             text: 'Home',
             href: '/'
         },
         {
-            text: 'Now',
-            href: '/now'
-        },
-        {
-            text: 'Words',
-            href: '/words'
-        },
-        {
             text: 'Projects',
             href: '/projects'
         },
-
+        {
+            text: 'Blog',
+            href: '/blog'
+        },
+        {
+            text: 'Tags',
+            href: '/tags'
+        }
     ],
     footerNavLinks: [
         {
@@ -91,7 +83,7 @@ const siteConfig: SiteConfig = {
     ],
     socialLinks: [
         {
-            text: 'LinkedIn',
+            text: 'Dribbble',
             href: 'https://dribbble.com/'
         },
         {
@@ -104,12 +96,12 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hello',
-        text: "I'm **Jessica Wu**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-        // image: {
-        //     src: '/hero.jpeg',
-        //     alt: 'A person sitting at a desk in front of a computer'
-        // }
+        title: 'Hi There & Welcome to My Corner of the Web!',
+        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        image: {
+            src: '/hero.jpeg',
+            alt: 'A person sitting at a desk in front of a computer'
+        },
         actions: [
             {
                 text: 'Get in Touch',
